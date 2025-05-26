144879efd1d5373498373b9bce90910b54f0ebcb
Tóth Kata <toth.kata@ckik.hu>
2025-05-26 17:53:11 +0200

    uhm ez miért nem működik

--- event.js
+++ event.js
@@ -1,6 +1,31 @@
 const sor = document.getElementById("sor").value
 const oszlop = document.getElementById("oszlop").value
+const tablazat = document.getElementById("tablazat")
+let palya = []
 
 function general() {
-    
+    // palyaGeneral()
+    for(let i = 0; i<sor; i++) {
+        let tr = document.createElement("tr")
+        for(let j = 0; j<oszlop; i++) {
+            let td = document.createElement("td")
+            tr.appendChild(td)
+        }
+        tablazat.appendChild(tr)
+    }
+}
+
+function random(a, f) {
+    return Math.floor(Math.random()*(f-a+1))+a;
+}
+
+function palyaGeneral() {
+    for(let i = 0; i<sor*oszlop; i++) {
+        let r = random(0,1)
+        if(r==0){
+            palya.push(false)
+        } else {
+            palya.push(true)
+        }
+    }
 }
